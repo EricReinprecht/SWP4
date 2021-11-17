d9d93707eeb9d52586f0253fcb8d8fe073aeda61
EricReinprecht <eric.reinprecht@student.htldornbirn.at>
2021-11-17 11:24:48 +0100

    funkt ne

--- TODO-List/index.js
+++ TODO-List/index.js
@@ -1,13 +1,20 @@
 var task;
-var person;
+var person = "";
 var divNumber = 1;
 var divNumberInput;
-var tasks = [];
+var tasksDiv = [];
+var tasks=[];
+
 
 
 function addTask(){
     task = document.getElementById("task").value;
     person = document.getElementById("person").value;
+    if(person!="name"){
+      person = "-" + person;
+    }else{
+      person = "";
+    }
     createDiv();
     divNumber++;
 }
@@ -15,17 +22,23 @@ function addTask(){
   function createDiv(){
     var container = document.getElementById('tasks');
     var newDiv = document.createElement('div');
-    newDiv.innerHTML = divNumber + ". " + task + "-" + person;
+    newDiv.innerHTML = divNumber + ". " + task + person;
+    tasks.push = divNumber + ". " + task + person; //funkt ne
     newDiv.setAttribute('id',divNumber);
-    tasks.push(newDiv);
+    tasksDiv.push(newDiv);
     container.appendChild(newDiv);
 }
 
-  function finishTask(){
+  function finishTask2(){
     divNumberInput = document.getElementById("taskNumber").value;
     var node = document.getElementById(divNumberInput);
     if (node.parentNode) {
         node.parentNode.removeChild(node);
     }
 }
-  
+
+function finishTask(){
+  divNumberInput = document.getElementById("taskNumber").value;
+  var node = document.getElementById(divNumberInput);
+  node.innerHTML = "<s>" + tasks[divNumberInput] + "</s>";
+}
