7fbe07fec8603e3fbd0cdeb0e8f8f77c2ac36f20
EricReinprecht <eric.reinprecht@student.htldornbirn.at>
2021-12-02 11:28:16 +0100

    stone pap sci fast fertig

--- Schere,Stein,Papier/index.js
+++ Schere,Stein,Papier/index.js
@@ -31,25 +31,87 @@ function getWinner() {
     }
 }
 
-function displayEndScreen(){
+function displayEndScreen() {
     var container = document.getElementById('main');
     var newDiv = document.createElement('div');
-    createContentOfEndScreen();
     newDiv.setAttribute('id', "endScreen");
     container.appendChild(newDiv);
+    createContentOfEndScreen();
 }
 
-function createContentOfEndScreen(){
+function createContentOfEndScreen() {
     var container = document.getElementById('endScreen');
-    var playersChoiseDiv = document.createElement('div');
-    var comChoiseDiv = document.createElement('div');
-    var result = document.createElement('div');
-    var restartButtun =document.createElement('div')
-    container.child.setAttribute('class', "endScreenContent");   //child is wrong
+    createDivs(container);
+    setClassOfDivs(container);
+    setInstructionOfDiv(container);
+    createContent();
 }
 
+function createContent() {
+    createResult();
+    createPlayerComChoise();
+    createRestartButton();
+}
 
+function createRestartButton() {
+    var div = document.getElementById('restartButton');
+    div.innerHTML = winner;
+}
 
+function createPlayerComChoise() {
+    var selector = playersChoise;
+    var div = document.getElementById();
+
+    switch (selector) {
+        case 1:
+            div.innerHTML = "<img src=rock.PNG>";
+            break;
+        case 2:
+            div.innerHTML = "<img src=scissors.PNG>";
+            break;
+        case 3:
+            div.innerHTML = "<img src=paper.PNG>";
+            break;
+    }
+}
+
+function createResult() {
+    var div = document.getElementById('result');
+    div.innerHTML = winner;
+}
+
+function createDivs(container) {
+    for (i = 0; i < 4; i++) {
+        var newDiv = document.createElement('div');
+        container.appendChild(newDiv);
+    }
+}
+
+function setClassOfDivs(container) {
+    for (let i = 0; i < container.children.length; i++) {
+        container.children[i].setAttribute('class', "endScreenContent")
+    }
+}
+
+function setInstructionOfDiv(container) {
+    for (let i = 0; i < container.children.length; i++) {
+        switch (i) {
+            case 0:
+                container.children[0].setAttribute('id', "restartButton")
+                container.children[0].setAttribute('onClick', "restartGame()")
+                break;
+            case 1:
+                container.children[1].setAttribute('id', "result")
+                break;
+            case 2:
+                container.children[2].setAttribute('id', "comChoise")
+                break;
+            case 3:
+                container.children[3].setAttribute('id', "playerChoise")
+                break;
+        }
+    }
+};
 
 
 
@@ -62,4 +124,6 @@ function setPlayersChoise(playersChoise) {
     this.playersChoise = playersChoise;
     play();
 }
+
+function restartGame() { }
 
