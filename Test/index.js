377a240c4f0d5c9ed4011b4b49d3bc0858010859
EricReinprecht <eric.reinprecht@student.htldornbirn.at>
2022-01-19 11:19:57 +0100

    Test Übung fertig

--- Test/index.js
+++ Test/index.js
@@ -1,37 +1,58 @@
 var size;
 var weight;
-var result = 5;
 var output;
 
 
 function doTheMagic() {
     getInputOfTask();
-    calculate(result);
+    var result = calculate(size, weight);
     getOutput(result);
-    console.log(output);
-    
 }
-  
+
 function getInputOfTask() {
     size = document.getElementById("size").value;
     weight = document.getElementById("weight").value;
 }
 
-function calculate(result){
-    result = ((size*1.43)/weight)-3,4;
-    console.log(result)
+function calculate(size, weight) {
+    return ((size * 1.43) / weight) - 3.4;
 }
 
-function getOutput(result){
-    if(result>0){
+function getOutput(result) {
+    checkIfPositiveOrNegative(result);
+}
+
+function checkIfPositiveOrNegative(result) {
+    if (result > 0) {
         output = "Positiv";
+        var numbers = "";
+        for (var number = -1; number < result; number++) {
+            numbers = numbers + getNumbers(number) + " < ";
+        }
     }
-    if(result<0){
+    if (result < 0) {
         output = "Negativ";
+        var numbers = "";
+        for (var number = -1; number > result; number--) {
+            numbers = numbers + getNumbers(number) + " < ";
+        }
     }
-    if(result = 0){
-        output = "Null;"
+    if (result == 0) {
+        output = "Null";
+        output = "Ergebins = " + result + ": " + output;
     }
-    
-
+    output = finishOutput(result, output, numbers);
+    document.getElementById("output").innerHTML = output;
 }
+
+function getNumbers(number) {
+    var numberString = number.toString();
+    return numberString;
+}
+
+function finishOutput(result, output, numbers){
+    return output = "Ergebins = " + result + ": " + output + ", Zahlenfolge: " + numbers + result;
+}
+
+
+
