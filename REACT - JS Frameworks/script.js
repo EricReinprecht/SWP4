561829303ed157e9ce02050d1afb42ccceb2a2de
EricReinprecht <eric.reinprecht@student.htldornbirn.at>
2022-03-03 10:08:36 +0100

    03 learnyounode

--- REACT - JS Frameworks/script.js
+++ REACT - JS Frameworks/script.js
@@ -1,4 +1,4 @@
 var sizeOf = require('image-size');
 
 var dimensions = sizeOf('C:\\Users\\reinp\\Pictures\\gatrmel.jpg')
-console.log(dimensions.width, dimensions.height)
+console.log(dimensions.width, dimensions.height) 
