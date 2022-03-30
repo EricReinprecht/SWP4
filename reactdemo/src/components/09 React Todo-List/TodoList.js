e284a519bc651a9a1c8032f3ad202ed05977ac97
EricReinprecht <eric.reinprecht@student.htldornbirn.at>
2022-03-30 10:12:14 +0200

    08 React Todo 2

--- reactdemo/src/components/09 React Todo-List/TodoList.js
+++ reactdemo/src/components/09 React Todo-List/TodoList.js
@@ -3,16 +3,20 @@ import styles from './TodoList.module.css';
 import Todo from './Todo';
 
 export default class TodoList extends Component{
+
+    getTodos(){
+        let widgets = [];
+        this.props.todos.forEach(todo =>{
+            widgets.push(<Todo title={todo.name} isDone={todo.done} />);
+        });
+        return widgets;
+    }
+
+
     render(){
         return(
             <div class={styles.TodoListMain}>
-                <Todo/>
-                <Todo/>
-                <Todo/>
-                <Todo/>
-                <Todo/>
-                <Todo/>
-                <Todo/>
+                {this.getTodos()}
             </div>
         );
     }
