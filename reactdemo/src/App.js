dd2cfda449efa80e3a5f231ad7237d146f0cc366
EricReinprecht <eric.reinprecht@student.htldornbirn.at>
2022-03-30 10:12:36 +0200

    08 React Todo 2

--- reactdemo/src/App.js
+++ reactdemo/src/App.js
@@ -1,21 +1,39 @@
 import { useCallback, useState } from 'react';
+import { Component } from 'react';
 import './App.css';
 import Header from './components/09 React Todo-List/Header';
 import TaskAdder from './components/09 React Todo-List/TaskAdder';
 import TodoList from './components/09 React Todo-List/TodoList';
 
+export default class Todo extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      todos:[
+        {
+          "id" : 1,
+          "name" : "einkaufen",
+          "done" : false
+        },
+        {
+          "id" : 2,
+          "name" : "putzen",
+          "done" : false
+        }
+      ] 
+    }
+  }
 
 
-function App() {
-  return (
-    <div className="app">
-      <div className='main'>
-        <Header/>
-        <TaskAdder/>
-        <TodoList/>
+  render(){
+    return (
+      <div className="app">
+        <div className='main'>
+          <Header/>
+          <TaskAdder/>
+          <TodoList todos ={this.state.todos}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
--- reactdemo/src/components/09 React Todo-List/Todo.js
+++ reactdemo/src/components/09 React Todo-List/Todo.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import { FaBeer } from 'react-icons/fa';
 import styles from './Todo.module.css';
 
+
+
 export default class Todo extends Component{
+    state = {   }
     render(){
         return(
             <div class={styles.TodoMain}>
-                <div>Einkaufen</div>
-                <div>Icon</div>
+                <div>{this.props.title}</div>
+                <div>{this.props.isDone}</div>
+                <div><FaBeer/></div>
             </div>
         );
     }
